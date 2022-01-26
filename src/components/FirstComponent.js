8d78f5951258ec30e996da3fd1da909feed8a8c8
shadansa <50177462+shadansa@users.noreply.github.com>
2022-01-26 13:09:27 +0000

    axios

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function FirstComponent() {
-  return <div>First Component In React - Guthub Page</div>;
+
+export class FirstComponent extends Component {
+  
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         persons: []
+      }
+    }
+    
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+          .then(response => {
+            const persons = response.data;
+            this.setState({ persons });
+          })
+    }
+  
+  
+    render() {
+        return (
+            <ul>
+                {
+                this.state.persons
+                    .map(person =>
+                    <li key={person.id}>{person.name}</li>
+                    )
+                }
+            </ul>
+        ) 
+        
+    }
 }
 
 export default FirstComponent;
